24841ac5da870ba20bae8e577cc71fbfc093b5f6
Jordan Piepkow <jpiepkow@gmail.com>
2019-11-06 21:36:40 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -e
-
+curl -sL https://github.com/digitalocean/doctl/releases/download/v1.33.1/doctl-v1.33.1-linux-amd64.tar.gz | tar -xzv
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
